0cb26122a710a34c16ae8b9bff62438400b10937
Ozan Honamlıoğlu <ozanhonamlioglu@hotmail.com>
2021-08-12 09:01:15 +0300

    widthOfString

---
+++ exts.swift
@@ -0,0 +1,9 @@
+extension String {
+    
+    func widthOfString(usingFont font: UIFont) -> CGFloat {
+        let fontAttribute = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttribute)
+        return size.width
+    }
+    
+}
